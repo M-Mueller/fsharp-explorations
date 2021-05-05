57dd44d9e51af266a1c99b06b54ce94614c52934
M-Mueller <mueller@imfusion.de>
2021-05-05 21:13:15 +0200

    Alternate roman numerals implementation

--- Katas/RomanNumerals.fs
+++ Katas/RomanNumerals.fs
@@ -1,55 +1,6 @@
 module RomanNumerals
 
-let rec internal parseLiterals (roman : list<char>) (arabic : list<int>) =
-    let convertSingle c =
-        match c with
-        | 'I' -> Ok 1
-        | 'V' -> Ok 5
-        | 'X' -> Ok 10
-        | 'L' -> Ok 50
-        | 'C' -> Ok 100
-        | 'D' -> Ok 500
-        | 'M' -> Ok 1000
-        | _ -> Error $"'{c}' is not a valid roman number"
-
-    let convertTuple (a, b) =
-        match (a, b) with
-        | ('I', 'V') -> Ok 4
-        | ('I', 'X') -> Ok 9
-        | ('X', 'L') -> Ok 40
-        | ('X', 'C') -> Ok 90
-        | ('C', 'M') -> Ok 900
-        | _ -> Error $"'{a}{b}' is not a valid roman number"
-
-    match roman with
-    | [] -> Ok arabic
-    | c1 :: c2 :: cs ->
-        match convertTuple (c1, c2) with
-        | Ok x -> parseLiterals cs (x :: arabic)
-        | Error _ ->
-            convertSingle c1
-            |> Result.map (fun x -> x :: arabic)
-            |> Result.bind (parseLiterals (c2 :: cs))
-    | c :: cs ->
-        convertSingle c
-        |> Result.map (fun x -> x :: arabic)
-        |> Result.bind (parseLiterals cs)
-
-let validate arabic =
-    let rec validateOrder remaining =
-        match remaining with
-        | [] -> Ok arabic
-        | d1 :: d2 :: _ when d1 > d2 -> Error "Smaller numbers cannot come before larger numbers"
-        | _ :: ds -> validateOrder ds
-
-    arabic |> validateOrder
-
-let toArabic (roman : string) =
-    parseLiterals (Seq.toList roman) []
-    |> Result.bind validate
-    |> Result.map (List.sum)
-
-let literals =
+let internal literals =
     [
         ("I", 1)
         ("IV", 4)
@@ -65,6 +16,46 @@ let literals =
         ("M", 1000)
     ]
 
+let internal literalsMap = Map literals
+
+let rec internal parseLiterals (roman : list<char>) (arabic : list<int>) =
+    let convertOne c =
+        let literal = string c
+
+        literalsMap.TryFind literal
+
+    let convertTwo c1 c2 =
+        let literal = string c1 + string c2
+
+        literalsMap.TryFind literal
+
+    let processRemaining c cs a =
+        match a with
+        | Some a -> parseLiterals cs (a :: arabic)
+        | None -> Error $"'{c}' is not a valid roman number"
+
+    match roman with
+    | [] -> Ok arabic
+    | c1 :: c2 :: cs ->
+        match convertTwo c1 c2 with
+        | Some a -> processRemaining c1 cs (Some a)
+        | None -> convertOne c1 |> processRemaining c1 (c2 :: cs)
+    | c :: cs -> convertOne c |> processRemaining c cs
+
+let validate arabic =
+    let rec validateOrder remaining =
+        match remaining with
+        | [] -> Ok arabic
+        | d1 :: d2 :: _ when d1 > d2 -> Error "Smaller numbers cannot come before larger numbers"
+        | _ :: ds -> validateOrder ds
+
+    arabic |> validateOrder
+
+let toArabic (roman : string) =
+    parseLiterals (Seq.toList roman) []
+    |> Result.bind validate
+    |> Result.map (List.sum)
+
 let fromArabic arabic =
     let rec convert roman arabic literals =
         match literals with
@@ -77,7 +68,4 @@ let fromArabic arabic =
     if arabic <= 0 then
         Error "Roman numbers must be larger than 0"
     else
-        literals
-        |> List.rev
-        |> convert "" arabic
-        |> Ok
+        literals |> List.rev |> convert "" arabic |> Ok
