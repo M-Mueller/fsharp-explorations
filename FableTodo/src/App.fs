e4075bea88e934ea7bb0c6cb41c47cc9ba7fc01b
M-Mueller <M-Mueller@users.noreply.github.com>
2021-11-06 21:38:56 +0100

    Use Guid and add filtering

--- FableTodo/src/App.fs
+++ FableTodo/src/App.fs
@@ -1,49 +1,55 @@
 module App
 
+open System
 open Feliz
 open Elmish
 open Elmish.React
 open Elmish.Debug
 open Elmish.HMR
 
-type Todo = { id: int; text: string; isDone: bool }
+type Todo =
+    { id: Guid
+      text: string
+      isDone: bool }
 
-type EditedTodo = { id: int; text: string }
+type EditedTodo = { id: Guid; text: string }
+
+[<RequireQualifiedAccess>]
+type Filter =
+    | All
+    | Completed
+    | NotCompleted
 
 type State =
     { newTodo: string
       todos: Todo list
-      editedTodo: EditedTodo option }
+      editedTodo: EditedTodo option
+      filter: Filter }
 
 let init () =
     { newTodo = ""
       todos = []
-      editedTodo = None }
+      editedTodo = None
+      filter = Filter.All }
 
 type Msg =
     | SetNewTodo of string
     | AddTodo
-    | ToggleTodo of int
-    | DeleteTodo of int
-    | EditTodo of int
+    | ToggleTodo of Guid
+    | DeleteTodo of Guid
+    | EditTodo of Guid
     | SetEditedText of string
     | CancelEditTodo
     | SaveEditTodo
+    | SetFilter of Filter
 
 let update (msg: Msg) (state: State) : State =
     match msg with
     | SetNewTodo text -> { state with newTodo = text }
     | AddTodo ->
-        let newId =
-            try
-                state.todos
-                |> List.maxBy (fun todo -> todo.id)
-                |> (fun todo -> todo.id + 1)
-            with _ -> 0
-
         { state with
               todos =
-                  { id = newId
+                  { id = Guid.NewGuid()
                     text = state.newTodo
                     isDone = false }
                   :: state.todos
@@ -91,106 +97,151 @@ let update (msg: Msg) (state: State) : State =
                                   { todo with text = editedTodo.text }
                               else
                                   todo) }
+    | SetFilter filter -> { state with filter = filter }
+
+
+let renderInput (currentText: string) (dispatch: Msg -> unit) =
+    Html.div [
+        prop.className "input-group my-2"
+        prop.children [
+            Html.input [
+                prop.className "form-control"
+                prop.valueOrDefault currentText
+                prop.onChange (fun s -> dispatch (SetNewTodo s))
+            ]
+            Html.button [
+                prop.className "btn btn-primary"
+                prop.disabled (String.IsNullOrEmpty currentText)
+                prop.children [
+                    Html.i [ prop.className "bi bi-plus" ]
+                ]
+                prop.onClick (fun _ -> dispatch AddTodo)
+            ]
+        ]
+    ]
+
+let renderFilters (activeFilter: Filter) (dispatch: Msg -> unit) =
+    let renderFilter (text: string) (filter: Filter) =
+        Html.button [
+            prop.className [
+                "btn"
+                if activeFilter = filter then
+                    "btn-primary"
+                else
+                    "btn-outline-primary"
+            ]
+            prop.onClick (fun _ -> dispatch (SetFilter filter))
+            prop.text text
+        ]
+
+    Html.div [
+        prop.className "btn-group w-100 my-2"
+        prop.children [
+            renderFilter "All" Filter.All
+            renderFilter "Completed" Filter.Completed
+            renderFilter "Not Completed" Filter.NotCompleted
+        ]
+    ]
+
+let renderTodo (todo: Todo) (dispatch: Msg -> unit) =
+    Html.li [
+        prop.className "card my-3"
+        prop.children [
+            Html.div [
+                prop.className "card-body d-flex flex-row"
+                prop.children [
+                    Html.span [
+                        prop.className "flex-grow-1 align-self-center mx-2 text-break"
+                        prop.text todo.text
+                    ]
+                    Html.button [
+                        prop.className [
+                            "btn"
+                            if todo.isDone then
+                                "btn-success"
+                            else
+                                "btn-outline-secondary"
+                            "align-self-center"
+                            "mx-2"
+                        ]
+                        prop.onClick (fun _ -> dispatch (ToggleTodo todo.id))
+                        prop.children [
+                            Html.i [ prop.className "bi bi-check" ]
+                        ]
+                    ]
+                    Html.button [
+                        prop.className "btn btn-warning align-self-center mx-2"
+                        prop.onClick (fun _ -> dispatch (EditTodo todo.id))
+                        prop.children [
+                            Html.i [
+                                prop.className "bi bi-pencil-square"
+                            ]
+                        ]
+                    ]
+                    Html.button [
+                        prop.className "btn btn-danger align-self-center mx-2"
+                        prop.onClick (fun _ -> dispatch (DeleteTodo todo.id))
+                        prop.children [
+                            Html.i [ prop.className "bi bi-trash" ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    ]
+
+let renderEditedTodo (todo: EditedTodo) (dispatch: Msg -> unit) =
+    Html.li [
+        prop.className "card my-3"
+        prop.children [
+            Html.div [
+                prop.className "card-body d-flex flex-row"
+                prop.children [
+                    Html.input [
+                        prop.className "flex-grow-1 align-self-center mx-2"
+                        prop.valueOrDefault todo.text
+                        prop.onChange (fun s -> dispatch (SetEditedText s))
+                    ]
+                    Html.button [
+                        prop.className "btn btn-primary align-self-center mx-2"
+                        prop.onClick (fun _ -> dispatch (SaveEditTodo))
+                        prop.children [
+                            Html.i [ prop.className "bi bi-save2" ]
+                        ]
+                    ]
+                    Html.button [
+                        prop.className "btn btn-secondary align-self-center mx-2"
+                        prop.onClick (fun _ -> dispatch (CancelEditTodo))
+                        prop.children [
+                            Html.i [ prop.className "bi bi-x" ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    ]
+
+let renderTodos (state: State) (dispatch: Msg -> unit) =
+    let filteredTodos =
+        state.todos
+        |> List.filter
+            (fun todo ->
+                match state.filter with
+                | Filter.All -> true
+                | Filter.Completed -> todo.isDone
+                | Filter.NotCompleted -> not todo.isDone)
+
+    Html.ul [
+        prop.className "list-unstyled"
+        prop.children [
+            for todo in filteredTodos ->
+                match state.editedTodo with
+                | Some editedTodo when editedTodo.id = todo.id -> renderEditedTodo editedTodo dispatch
+                | _ -> renderTodo todo dispatch
+        ]
+    ]
 
 let render (state: State) (dispatch: Msg -> unit) =
-    let renderInput =
-        Html.div [
-            prop.className "input-group"
-            prop.children [
-                Html.input [
-                    prop.className "form-control"
-                    prop.valueOrDefault state.newTodo
-                    prop.onChange (fun s -> dispatch (SetNewTodo s))
-                ]
-                Html.button [
-                    prop.className "btn btn-primary"
-                    prop.disabled (System.String.IsNullOrEmpty state.newTodo)
-                    prop.children [
-                        Html.i [ prop.className "bi bi-plus" ]
-                    ]
-                    prop.onClick (fun _ -> dispatch AddTodo)
-                ]
-            ]
-        ]
-
-    let renderTodo (todo: Todo) =
-        Html.li [
-            prop.className "card my-3"
-            prop.children [
-                Html.div [
-                    prop.className "card-body d-flex flex-row"
-                    prop.children [
-                        Html.span [
-                            prop.className "flex-grow-1 align-self-center mx-2 text-break"
-                            prop.text todo.text
-                        ]
-                        Html.button [
-                            prop.className [
-                                "btn"
-                                if todo.isDone then
-                                    "btn-success"
-                                else
-                                    "btn-outline-secondary"
-                                "align-self-center"
-                                "mx-2"
-                            ]
-                            prop.onClick (fun _ -> dispatch (ToggleTodo todo.id))
-                            prop.children [
-                                Html.i [ prop.className "bi bi-check" ]
-                            ]
-                        ]
-                        Html.button [
-                            prop.className "btn btn-warning align-self-center mx-2"
-                            prop.onClick (fun _ -> dispatch (EditTodo todo.id))
-                            prop.children [
-                                Html.i [
-                                    prop.className "bi bi-pencil-square"
-                                ]
-                            ]
-                        ]
-                        Html.button [
-                            prop.className "btn btn-danger align-self-center mx-2"
-                            prop.onClick (fun _ -> dispatch (DeleteTodo todo.id))
-                            prop.children [
-                                Html.i [ prop.className "bi bi-trash" ]
-                            ]
-                        ]
-                    ]
-                ]
-            ]
-        ]
-
-    let renderEditedTodo (todo: EditedTodo) =
-        Html.li [
-            prop.className "card my-3"
-            prop.children [
-                Html.div [
-                    prop.className "card-body d-flex flex-row"
-                    prop.children [
-                        Html.input [
-                            prop.className "flex-grow-1 align-self-center mx-2"
-                            prop.valueOrDefault todo.text
-                            prop.onChange (fun s -> dispatch (SetEditedText s))
-                        ]
-                        Html.button [
-                            prop.className "btn btn-primary align-self-center mx-2"
-                            prop.onClick (fun _ -> dispatch (SaveEditTodo))
-                            prop.children [
-                                Html.i [ prop.className "bi bi-save2" ]
-                            ]
-                        ]
-                        Html.button [
-                            prop.className "btn btn-secondary align-self-center mx-2"
-                            prop.onClick (fun _ -> dispatch (CancelEditTodo))
-                            prop.children [
-                                Html.i [ prop.className "bi bi-x" ]
-                            ]
-                        ]
-                    ]
-                ]
-            ]
-        ]
-
     Html.div [
         prop.className "container my-3"
         prop.style [
@@ -198,21 +249,12 @@ let render (state: State) (dispatch: Msg -> unit) =
         ]
         prop.children [
             Html.h1 "To-Do List"
-
-            renderInput
-
-            Html.ul [
-                prop.className "list-unstyled"
-                // prop.children (List.map renderTodo state.todos)
-                prop.children [
-                    for todo in state.todos ->
-                        match state.editedTodo with
-                        | Some editedTodo when editedTodo.id = todo.id -> renderEditedTodo editedTodo
-                        | _ -> renderTodo todo
-                ]
-            ]
+            renderInput state.newTodo dispatch
+            renderFilters state.filter dispatch
+            renderTodos state dispatch
         ]
     ]
+
 
 Program.mkSimple init update render
 |> Program.withReactSynchronous "elmish-app"
